7f1a11139aa15ec3c92e7b5df1a7a2ab3249e057
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-07-18 22:22:20 +0100

    should add the next two throws when the previous is an strike

--- test/bowling_game_spec.rb
+++ test/bowling_game_spec.rb
@@ -7,6 +7,8 @@ def points(line)
       result += 0
     elsif line[index] == '/'
       result += 10 + line[index + 1].ord - 48
+    elsif line[index] == 'X'
+      result += 10 + line[index + 1].ord - 48 + line[index + 2].ord - 48
     else
       result += line[index].ord - 48
     end
@@ -30,5 +32,9 @@ describe 'BowlingGameShould' do
 
   it 'should add the next throw when the previous is an spare' do
     expect(points('1---/4---------------')).to(eq(19))
+  end
+
+  it 'should add the next two throws when the previous is an strike' do
+    expect(points('1---X45--------------')).to(eq(29))
   end
 end
