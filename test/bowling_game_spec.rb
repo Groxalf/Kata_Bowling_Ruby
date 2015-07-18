0292ff937c6c764b6cfaf288a86b33c165fd9495
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-07-18 21:55:47 +0100

    green: should return 1 points when the first bowl is the only drawn in a bowling line

--- test/bowling_game_spec.rb
+++ test/bowling_game_spec.rb
@@ -1,12 +1,19 @@
 require 'rspec'
 
-  def points(line)
-    0
+def points(line)
+  if line[0] == '-'
+    return 0
   end
+  return line[0].ord - 48
+end
 
 describe 'BowlingGameShould' do
 
   it 'should return zero points when no drawn in a bowling line' do
     expect(points('---------------------')).to(eq(0))
+  end
+
+  it 'should return 1 points when the first bowl is the only drawn in a bowling line' do
+    expect(points('1--------------------')).to(eq(1))
   end
 end
